ef4cec20995396b86e3315dd6ffba02e68b4ec7e
Пользователь <polina.mumber@gmail.com>
2023-10-31 05:46:32 +0600

    font1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 
 import { AppRoutes } from './routes';
-<AppRoutes  />
-
+const GlobalStyle =createGlobalStyle `
+   * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+   }   
+   html,
+   body {
+      width: 100%;
+      height: 100%;
+      font-family: 'StratosSkyeng', sans-serif;
+      color: #FFFFFF;
+   }
+   `
 function App() {
   return (
     <AppRoutes  />
