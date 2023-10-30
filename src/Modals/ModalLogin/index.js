031f3020f0044fabf4ebe267e4fb075024c3f68c
Alexandrov Georgy <g.aleksandrov116@yandex.ru>
2023-10-30 11:39:03 +0300

    added registr form

--- src/Modals/ModalLogin/index.js
+++ src/Modals/ModalLogin/index.js
@@ -5,38 +5,83 @@ import { Box, Modal, TextField } from '@mui/material/';
 import logoBlack from '../../img/logoBlack.png';
 
 const ModalLogin = ({ open, setOpen }) => {
-  const handleClose = () => setOpen(false);
+  const [openModalRegistration, setOpenModalRegistration] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+    setOpenModalRegistration(false);
+  };
 
   return (
     <div className={S.modal}>
-      <Modal
-        sx={{ left: 'calc(50% - 183px)', marginTop: '200px' }}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
-        <Box className={S.modalBox}>
-          <img
-            src={logoBlack}
-            alt="logo"
-          />
-          <TextField
-            className={S.inputBox}
-            sx={{ width: '278px', fontSize: '18px' }}
-            id="standard-basic"
-            label="Логин"
-            variant="standard"
-          />
-          <TextField
-            sx={{ width: '278px', marginTop: '20px' }}
-            id="standard-basic"
-            label="Пароль"
-            variant="standard"
-          />
-          <button className={S.buttonEntre}>Войти</button>
-          <button className={S.buttonReg}>Зарегистрироваться</button>
-        </Box>
-      </Modal>
+      {!openModalRegistration ? (
+        <Modal
+          sx={{ left: 'calc(50% - 183px)', marginTop: '200px' }}
+          open={open}
+          onClose={() => handleClose()}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
+          <Box className={S.modalBox}>
+            <img
+              src={logoBlack}
+              alt="logo"
+            />
+            <TextField
+              className={S.inputBox}
+              sx={{ width: '278px', fontSize: '18px' }}
+              id="standard-basic"
+              label="Логин"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: '278px', marginTop: '20px' }}
+              id="standard-basic"
+              label="Пароль"
+              variant="standard"
+            />
+            <button className={S.buttonEntre}>Войти</button>
+            <button
+              onClick={() => setOpenModalRegistration(true)}
+              className={S.buttonReg}>
+              Зарегистрироваться
+            </button>
+          </Box>
+        </Modal>
+      ) : (
+        <Modal
+          sx={{ left: 'calc(50% - 183px)', marginTop: '200px' }}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
+          <Box className={S.modalBox}>
+            <img
+              src={logoBlack}
+              alt="logo"
+            />
+            <TextField
+              className={S.inputBox}
+              sx={{ width: '278px', fontSize: '18px' }}
+              id="standard-basic"
+              label="Логин"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: '278px', marginTop: '20px' }}
+              id="standard-basic"
+              label="Пароль"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: '278px', marginTop: '20px' }}
+              id="standard-basic"
+              label="Повторите пароль"
+              variant="standard"
+            />
+            <button className={S.buttonEntre}>Зарегистрироваться</button>
+          </Box>
+        </Modal>
+      )}
     </div>
   );
 };
